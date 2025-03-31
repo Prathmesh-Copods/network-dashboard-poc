d2c19f281bc8c6375debd6668e27fc76cf45ee10
Prathmesh-Copods <prathmesh.dhatrak@copods.co>
2025-03-31 15:08:44 +0530

    build: add d3 library and update path configurations
    Add d3 library and its types to the project dependencies. Update tsconfig.app.json and vite.config.ts to include new path mappings for services, types, and pages directories. This change enables better module resolution and enhances the project's ability to handle data visualization tasks.

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig(() => {
         app: resolve(__dirname, "src", "app"),
         components: resolve(__dirname, "src", "components"),
         hooks: resolve(__dirname, "src", "hooks"),
+        services: resolve(__dirname, "src", "services"),
+        types: resolve(__dirname, "src", "types"),
+        pages: resolve(__dirname, "src", "pages"),
       },
     },
   };
